27cf3a5a4005444c3bbcb45386c720c1e14a56df
malte clement <malteclement@web.de>
2023-12-07 23:23:11 +0100

    major refactoring

--- Haskell/Day7/A.hs
+++ Haskell/Day7/A.hs
@@ -1,14 +1,15 @@
 import Text.Parsec ( parse, Parsec, many1, alphaNum, char, many, digit, sepBy)
 import Data.Functor ( ($>) )
+import Data.Functor.Classes ( liftCompare, Ord1 (liftCompare) )
 import Data.List (sortBy, sort, nub, sortBy, group, find, partition)
 import Data.Maybe (fromMaybe, listToMaybe)
 import Data.Function (on)
 
 main1 :: IO ()
-main1 = readFile "./input.txt"  >>= \s -> either print (print . solve cmp) (parse inputP "" s)
+main1 = readFile "./input.txt"  >>= \s -> either print (print . solve (cmp value score)) (parse inputP "" s)
 
 main2 :: IO ()
-main2 = readFile "./input.txt"  >>= \s -> either print (print . solve cmpJ) (parse inputP "" s)
+main2 = readFile "./input.txt"  >>= \s -> either print (print . solve (cmp valueJ scoreJ)) (parse inputP "" s)
 
 type Parser = Parsec String ()
 
@@ -26,33 +27,24 @@ roundP = Round <$> many1 alphaNum <*> (spaces *> (read <$> many1 digit))
 spaces :: Parser ()
 spaces = many (char ' ') $> ()
 
-cmp :: Round -> Round -> Ordering
-cmp r1 r2 = case compare (score $ hand r1) (score $ hand r2) of
-    EQ -> head $ filter (/= EQ) (zipWith (cmpLetter toInt) (hand r1) (hand r2))
-    x  -> x
+cmp :: [(Char, Int)] -> ([Char] -> Int) -> Round -> Round -> Ordering
+cmp m sc r1 r2 = liftCompare (liftCompare (compare `on` toIntG m)) (sc $ hand r1, hand r1) (sc $ hand r2, hand r2)
 
 score :: [Char] -> Int
 score cs = let (x:xs) = sortBy (flip compare `on` length) . group . sort $ cs
            in length x * 10 + if null xs then 0 else length (head xs)
 
-cmpLetter :: (Char -> Int) -> Char -> Char -> Ordering
-cmpLetter f c1 c2 = f c1 `compare` f c2
-
-toInt :: Char -> Int
-toInt c = fromMaybe (read [c]) $ lookup c [('T', 10), ('J', 11), ('Q', 12), ('K', 13), ('A', 14)]
+toIntG :: [(Char, Int)] -> Char -> Int
+toIntG m c = fromMaybe (read [c]) $ lookup c m 
 
 solve :: (Round -> Round -> Ordering) -> [Round] -> Int
 solve comp = sum . zipWith (*) [1..] . map bid . sortBy comp 
-
-cmpJ :: Round -> Round -> Ordering
-cmpJ r1 r2 = case compare (scoreJ $ hand r1) (scoreJ $ hand r2) of
-    EQ -> head $ filter (/= EQ) (zipWith (cmpLetter toIntJoker) (hand r1) (hand r2))
-    x  -> x
 
 scoreJ :: [Char] -> Int
 scoreJ cs = let (js, cs') = partition (=='J') cs 
                 (x : xs)  = sortBy (flip compare `on` length) . group . sort $ cs'
             in if length js == 5 then 50 else (length x + length js) * 10 + if null xs then 0 else length (head xs)
 
-toIntJoker :: Char -> Int
-toIntJoker c = fromMaybe (read [c]) $ lookup c [('T', 10), ('J', 1), ('Q', 12), ('K', 13), ('A', 14)]
+valueJ, value :: [(Char, Int)]
+value = [('T', 10), ('J', 11), ('Q', 12), ('K', 13), ('A', 14)]
+valueJ = [('T', 10), ('J', 1), ('Q', 12), ('K', 13), ('A', 14)]
